1978ead5de68f81cfbb5afe17f3decdd77913755
John H Wallace <john.wallace@hamnavoepartners.com>
2021-03-09 11:59:04 +0000

    Initial commit

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    // enabled: true, //comment this in to see purge in development
+    content: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue']
+    // Add any other JS files here (i.e. .jsx, .ts, etc...)
+    },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
