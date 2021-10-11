372fe9602d64b72ebe7e523c19f2573d81a231b7
John H Wallace <john.wallace@hamnavoepartners.com>
2021-10-11 22:39:45 +0100

    add additional name to with us list

--- app/jobs/update_issue_with_customer_state_job.rb
+++ app/jobs/update_issue_with_customer_state_job.rb
@@ -7,7 +7,7 @@ class UpdateIssueWithCustomerStateJob < ApplicationJob
   def perform(project_id)
     issues = Issue.where(project_id: project_id)
     issues.each do |issue|
-      with_us = issue.assigned_to.in?(["Pramod", "Victor", "Conrad", "HenryF", "John Wallace", "Gayathri", "Wim Rosier", "Daniel Musumeci", "Karthish", "Morgan Lynch"])
+      with_us = issue.assigned_to.in?(["Edson", "Pramod", "Victor", "Conrad", "HenryF", "John Wallace", "Gayathri", "Wim Rosier", "Daniel Musumeci", "Karthish", "Morgan Lynch"])
       issue.update(with_customer: !with_us)
     end
   end
