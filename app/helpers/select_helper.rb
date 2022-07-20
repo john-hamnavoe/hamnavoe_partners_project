f719071809b8fa405c1f9bdc9cfed5ce1162c7c9
John H Wallace <john.wallace@hamnavoepartners.com>
2022-07-20 12:06:36 +0100

    fix order by on test case select helper

--- app/helpers/select_helper.rb
+++ app/helpers/select_helper.rb
@@ -2,7 +2,7 @@
 
 module SelectHelper
   def test_case_for_select
-    TestCase.where(project: Project.first).map { |test_case| ["#{test_case.position}. #{test_case.title}", test_case.id] }
+    TestCase.where(project: Project.first).order(:position).map { |test_case| ["#{test_case.position}. #{test_case.title}", test_case.id] }
   end
 end
 
